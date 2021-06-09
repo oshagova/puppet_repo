03935fc99b471fc1b4706ae52bed8c7acde66897
oshagova <84322506+oshagova@users.noreply.github.com>
2021-06-09 22:48:37 +0300

    Update sites.pp

--- manifests/sites.pp
+++ manifests/sites.pp
@@ -1,1 +1,63 @@
-# puppet_repo
+node default {
+}
+
+node 'slave1.puppet' {
+
+package { 'httpd': ensure => 'installed' }
+
+service { 'httpd':
+  ensure => running,
+  enable => true,
+        }
+
+file { '/root/README':
+      ensure => absent,
+     }
+
+file { '/var/www/site1/index.html':
+      ensure => file,
+      source => '/vagrant/index.html',
+      replace => true,
+      }
+
+file { '/etc/httpd/conf.d/site1.conf':
+      ensure => file,
+      source => '/vagrant/site1.conf',
+      replace => true,
+      }
+
+service { 'httpd':
+  ensure => restarted,
+        }
+}
+
+node 'slave2.puppet' {
+
+$packages = [ 'httpd', 'php' ]
+package { $packages: ensure => 'installed' }
+
+service { 'httpd':
+  ensure => running,
+  enable => true,
+        }
+
+file { '/root/README':
+      ensure => absent,
+     }
+
+file { '/var/www/site2/index.php':
+      ensure => file,
+      source => '/vagrant/index.php',
+      replace => true,
+      }
+
+file { '/etc/httpd/conf.d/site2.conf':
+      ensure => file,
+      source => '/vagrant/site2.conf',
+      replace => true,
+      }
+
+service { 'httpd':
+  ensure => restarted,
+        }
+}        
